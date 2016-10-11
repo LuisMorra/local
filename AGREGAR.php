19bad0cf99dbf377cb36904910d73ef7ee215e8b
LuisMorra <morareylu@gmail.com>
2016-10-11 05:23:49 -0400

    Add files via upload

---
+++ AGREGAR.php
@@ -0,0 +1,21 @@
+<html>
+<head><title>AGREGAR</title></head>
+
+<body background="imagenes/agregar/meganamor.jpg">
+<form name="form1" method='POST'>  
+<input name="peliculas" type="button" value="PELICULAS" onClick="location.href='peliculas.php'"> 
+<input name="peliblue" type="button" value="PELISBLUEY" onClick="location.href='pelis'">
+<input name="juegos" type="button" value="JUEGOS" onClick="location.href='juegos.php'"> 
+<input name="software" type="button" value="SOFTWARE" onClick="location.href='software.php'"> 
+<input name="documentales" type="button" value="DOCUMENTALES" onClick="location.href='DOCUMENTALES.php'"> 
+<input name="variado" type="button" value="VARIADO" onClick="location.href='variado.php'"> 
+<input name="drivers" type="button" value="DRIVERS" onClick="location.href='drivers.php'">  
+<input name="series" type="button" value="SERIES" onClick="location.href='series.php'"> 
+<input name="mp3yvideos" type="button" value="MP3/VIDEOS" onClick="location.href='mp3yvideos.php'"> 
+<input name="salir" type="button" value="MENU PRINCIPAL" onClick="location.href='paginaprincipal.php'">
+</form>
+<?php
+echo "<br><br><br><br><br><br><br><br>";
+?>
+</body>
+</html>
